2caf747c6703894c72a0fb8f985623dba8b92641
said017 <saidam017@gmail.com>
2022-09-19 20:28:50 +0700

    finish from the basic app

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,27 @@
 import React, { useContext, useState } from "react";
 import { Col, Container, Form, Row, Button, Spinner } from "react-bootstrap";
-// import { useLoginUserMutation } from "../services/appApi";
+import { useLoginUserMutation } from "../services/appApi";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
-// import { AppContext } from "../context/appContext";
+import { AppContext } from "../context/appContext";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  //   const { socket } = useContext(AppContext);
-  //   const [loginUser, { isLoading, error }] = useLoginUserMutation();
+  const { socket } = useContext(AppContext);
+  const [loginUser, { isLoading, error }] = useLoginUserMutation();
   function handleLogin(e) {
     e.preventDefault();
     // login logic
-    // loginUser({ email, password }).then(({ data }) => {
-    //   if (data) {
-    //     // socket work
-    //     socket.emit("new-user");
-    //     // navigate to the chat
-    //     navigate("/chat");
-    //   }
-    // });
+    loginUser({ email, password }).then(({ data }) => {
+      if (data) {
+        // socket work
+        socket.emit("new-user");
+        // navigate to the chat
+        navigate("/chat");
+      }
+    });
   }
 
   return (
@@ -34,7 +34,7 @@ function Login() {
         >
           <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleLogin}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              {/* {error && <p className="alert alert-danger">{error.data}</p>} */}
+              {error && <p className="alert alert-danger">{error.data}</p>}
               <Form.Label>Email address</Form.Label>
               <Form.Control
                 type="email"
@@ -59,8 +59,7 @@ function Login() {
               />
             </Form.Group>
             <Button variant="primary" type="submit">
-              {/* {isLoading ? <Spinner animation="grow" /> : "Login"} */}
-              Login
+              {isLoading ? <Spinner animation="grow" /> : "Login"}
             </Button>
             <div className="py-4">
               <p className="text-center">
